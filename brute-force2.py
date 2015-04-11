a76c538b450cfad6078a240a23692baff6dd2a05
Brienna Herold <brienna.herold@gmail.com>
2015-04-11 19:59:38 -0400

    Added brute-force2.py

---
+++ brute-force2.py
@@ -0,0 +1,16 @@
+# Modify program 1 to output the total weight of each of these combinations 
+# of coins. To do this, you will need to figure out how much pennies, 
+# nickels, dimes, and quarters actually weigh, because I don't know this. 
+
+# Time: 3 minutes
+
+amount = 25.00
+
+for a in range(amount/.01):													
+	for b in range(amount/.05): 											
+		for c in range(amount/.10):											
+			for d in range(amount/.25):										
+				if .01*a + .05*b + .10*c + .25*d == amount:					
+					print (a*2.500) + (b*5.000) + (c*2.268) + (d*5.670)
+					
+# Weight info is from the U.S. Mint's website			
